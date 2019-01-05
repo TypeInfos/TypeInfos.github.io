d3396f2d72a9909d51a0095acae41d1f6e8b87d0
陈金伙 <1443970739@qq.com>
2019-01-06 00:16:44 +0800

    push

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,21 +18,21 @@ module.exports = {
         title: 'JavaScript', // 侧边栏名称
         collapsable: true, // 可折叠
         children: [
-          '/blog/JavaScript/学会了ES6，就不会写出那样的代码', // 你的md文件地址
+          '', // 你的md文件地址
         ]
       },
       {
         title: 'CSS',
         collapsable: true,
         children: [
-          '/blog/CSS/搞懂Z-index的所有细节',
+          '',
         ]
       },
       {
         title: 'HTTP',
         collapsable: true,
         children: [
-          '/blog/HTTP/认识HTTP-Cookie和Session篇',
+          '',
         ]
       },
     ]
